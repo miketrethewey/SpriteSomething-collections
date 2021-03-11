fed0914dae785db5d8f51bed0640c9b3d1f3d980
Mike A. Trethewey <miketrethewey@gmail.com>
2021-03-10 20:55:08 -0800

    Update CI

--- resources/ci/common/cleanup.py
+++ resources/ci/common/cleanup.py
@@ -3,9 +3,9 @@ import os
 from shutil import rmtree
 
 toNuke = [
-  os.path.join(".",".git"),
-	os.path.join(".",".github"),
-	os.path.join(".","resources")
+#  os.path.join(".",".git"), # leave it as a repo to run git commands later
+	os.path.join(".",".github"), # nuke workflows
+	os.path.join(".","resources") # nuke py source
 ]
 for nuke in toNuke:
   rmtree(nuke)
