038c97071d549a1d4fe85698dbcaae2bc9070632
Mike A. Trethewey <miketrethewey@gmail.com>
2021-03-10 20:45:39 -0800

    Prepare to manually push to GH Pages

--- resources/ci/common/cleanup.py
+++ resources/ci/common/cleanup.py
@@ -3,7 +3,9 @@ import os
 from shutil import rmtree
 
 toNuke = [
-  os.path.join(".",".git")
+  os.path.join(".",".git"),
+	os.path.join(".",".github"),
+	os.path.join(".","resources")
 ]
 for nuke in toNuke:
   rmtree(nuke)
