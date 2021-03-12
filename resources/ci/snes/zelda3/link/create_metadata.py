e159ec73080b109a42dd7879fd230fca06e1a558
Mike A. Trethewey <miketrethewey@gmail.com>
2021-03-12 03:28:27 -0800

    Sort JSON output

--- resources/ci/snes/zelda3/link/create_metadata.py
+++ resources/ci/snes/zelda3/link/create_metadata.py
@@ -118,4 +118,4 @@ for sprite in sprites:
   i += 1
 
 with(open(os.path.join(site_resources,"sprites-meta.json"),"w+")) as json_file:
-    json.dump(list(spritesmeta.values()),json_file,indent=2)
+    json.dump(sorted(list(spritesmeta.values()), key=lambda s: str.lower(s["slug"] or "").strip()),json_file,indent=2)
